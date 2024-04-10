2fb3a1350ef77a6fa9ced339c5502e1e737771b1
Juan Carlos Saucedo <82184172+juancarlossaucedo@users.noreply.github.com>
2024-04-10 12:09:13 -0500

    consumo_vuelo

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,6 @@
 export const API_CONFIG = {
   baseUrl: "https://django-rest-starter-zyi6-production.up.railway.app/api/",
+
+  //Vuelo
+  vuelo: "Vuelo/",
 };
--- src/app/services/conf/conf.service.ts
+++ src/app/services/conf/conf.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { API_CONFIG } from '../..//config/api.config';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http'; // Agrega esta línea para importar HttpHeaders
 import { Observable } from 'rxjs'; // Agrega esta línea para importar Observable
+import { AuthService } from '../auth/auth.service';
+import { map, catchError } from 'rxjs/operators'; // Agrega estas líneas para importar map y catchError
+
 @Injectable({
   providedIn: 'root'
 })
 export class ConfService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient , private authService: AuthService) { }
 
   // CRUD TIPO DOC
   listarTiposDocumento(): Observable<any> {
@@ -21,7 +25,23 @@ export class ConfService {
 
   //CRUD TIPO VUELO
   listarVuelos(): Observable<any> {
-    return this.http.get<any>(`${API_CONFIG.baseUrl}Vuelo/`);
+    const token = this.authService.getToken();
+    console.log(token);
+    const headers = new HttpHeaders({
+      Authorization: `token ${token}`,
+    });
+    return this.http.get<any>(`${API_CONFIG.baseUrl}${API_CONFIG.vuelo}` , { headers })
+    .pipe(
+      map((response: any) => {
+        console.log(response);
+        return response;
+      }),
+      catchError((error) => {
+        console.error("Error al obtener los datos del usuario", error);
+        return error;
+      })
+    );
+
   }
 
   eliminarVuelos(id: number): Observable<any> {
