ca80bae4cdfb0f6842c887c6ad8c0b9ebcb730f6
Cris_Lara <82177927+CristianLara-blip@users.noreply.github.com>
2024-05-29 02:50:33 -0500

    e2e

--- cypress/e2e/my-first-test.cy.ts
+++ cypress/e2e/my-first-test.cy.ts
@@ -1,16 +1,32 @@
 // cypress/integration/app.spec.js
 describe('Mi Primera Prueba', () => {
-    before(() => {
+    beforeEach(() => {
         cy.visit('http://localhost:4200')
-    })
+      });
     
-    it('El botón es visible', () => {
-        cy.contains('Iniciar Sesión');  
-    })
+    it('debería iniciar sesión con credenciales invalidas', () => {
+        // Ingresar credenciales válidas
+        cy.get('input[name="email"]').type('user@gmail.com');
+        cy.get('input[name="password"]').type('user12345');
+    
+        // Enviar el formulario
+        cy.get('button[type="submit"]').click();
 
-    it('EL enlace es visible', () => {
-        cy.visit('http://localhost:4200')
-        cy.contains('¿Has olvidado tu contraseña?');  
-    })
+         // Verificar que se muestre un mensaje de error
+    cy.contains('Oops...').should('be.visible');
+    cy.contains('¡Algo salió mal!').should('be.visible');
+
+    });
+
+
+    it('debería iniciar sesión con credenciales válidas', () => {
+        // Ingresar credenciales válidas
+        cy.get('input[name="email"]').type('zetagdcl@gmail.com');
+        cy.get('input[name="password"]').type('holamundo12345');
+    
+        // Enviar el formulario
+        cy.get('button[type="submit"]').click();
+    });
+    
   });
   
